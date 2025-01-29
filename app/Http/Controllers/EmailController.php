454019842775126ada5e1257344391d184f49c66
harshsshah2001 <harshsshah2001@gmail.com>
2025-01-29 12:23:14 +0530

    add data in database

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\CustomerController;
+use App\Models\Customer;
 use App\Mail\TestEmail;
 use Exception;
 use Illuminate\Http\Request;
@@ -17,29 +19,45 @@ class EmailController extends Controller
     }
     public function send(Request $request)
     {
-        // Assuming 'to_email' and 'from_email' are passed from middleware or request
+
+
+       $validate = $request->validate([
+            'from_mail' => 'required|email',
+            'to_mail' => 'required|email',
+            'add_comment' => 'required|string',
+        ]);
+
+        $fromEmail = $validate['from_mail'];
+        $toEmail = $validate['to_mail'];
+        $add_comment = $validate['add_comment'];
+
+
         try {
+
+            Customer::create([
+                'from_mail' => $fromEmail,
+                'to_mail' => $toEmail,
+                'add_comment' => $add_comment,
+            ]);
+
             // dd($request);
             $fromEmail = $request->from_mail;
-            $toEmail = $request->to_mail;
+            $toEmail = $request->to_mail;   
             $add_comment = $request->add_comment;
 
-            // Mail::to($toEmail)->send(new TestEmail());
-            // Mail::to($toEmail)->send(new TestEmail());
-            // ->from($fromEmail) // Set the sender's email address
+
             $data = [
                 'content' => $add_comment
-                // other data...
+
             ];
             Mail::send('emails.test', $data, function ($message) use ($request) {
-                $message->from($request->from_mail, 'Manish'); // Use from_email from request
+                $message->from($request->from_mail, 'harsh');
                 $message->to($request->to_mail)->subject('Test Email Subject1234');
             });
 
             return back()->with('success', 'Email sent successfully!');
 
 
-            // return response()->json(['message' => 'Email sent successfully!']);
         } catch (Exception $e) {
             logger($e->getMessage());
         }
